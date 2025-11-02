00f664878610176a36086322ca40585732eb4204
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-02 02:45:43 -0500

    Add Lesson_13_Understanding_Spark_Python_Requirements/part_3_setup_spark_environment.bash

---
+++ Lesson_13_Understanding_Spark_Python_Requirements/part_3_setup_spark_environment.bash
@@ -0,0 +1,5 @@
+export SPARK_HOME=/opt/spark
+export PATH=$SPARK_HOME/bin:$PATH
+export PYSPARK_PYTHON=python3   # Replace with your Python binary
+export PYSPARK_DRIVER_PYTHON=jupyter
+source ~/.bashrc
